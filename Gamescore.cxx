d09447b5f619bf753bd20eef6186a6addcb0a1a4
SilentWildcard <pg5636626@gmail.com>
2026-06-06 21:53:18 +0530

    Add Gamescore.cxx

---
+++ Gamescore.cxx
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <string>
+using namespace std; 
+int main() { 
+      string Title = "Priya Gupta";
+      cout << Title<<endl;
+      int gamescore = 50 ;
+      cout << gamescore<<endl;
+      return 0;
+ } 
