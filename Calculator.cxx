4384f760a8860ef5b0f163ba480cf81c06750c08
SilentWildcard <pg5636626@gmail.com>
2026-06-09 22:57:03 +0530

    Add Calculator.cxx

---
+++ Calculator.cxx
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+double x , y;
+double sum;
+cout << "Type a number:";
+cin >> x;
+
+cout << "Type another number:";
+cin >> y; 
+
+sum = x + y;
+cout << "Sum is:" << sum;
+return 0;
+}
